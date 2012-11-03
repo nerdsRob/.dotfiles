3690bb4ed498cafce811e6fe7fb1fd73f53dce4d
Robbb <robsiwek@airrob.speedport_w_723v_1_26_000>
2012-11-03 23:02:53 +0100

    ls -la

--- etc/aliases.zsh
+++ etc/aliases.zsh
@@ -1,3 +1,4 @@
+alias lsa="ls -la"
 alias webserver='python -m SimpleHTTPServer'
 alias rs-logo='print -P "%F{120}" && cat ~/.dotfiles/ascii-art/rs-logo && print -P "%f"'
 alias soundcloud-logo='print -P "%F{172}" && cat ~/.dotfiles/ascii-art/soundcloud && print -P "%f"'
