4720bbb51d6bb232fe4a0f8d50f475e5b59fcc9d
nerdsRob <robert@soundcloud.com>
2013-03-22 21:42:46 +0100

    Remove 'Open with...' duplicates

--- etc/aliases.zsh
+++ etc/aliases.zsh
@@ -3,3 +3,5 @@ alias ll="ls -la"
 alias webserver='python -m SimpleHTTPServer'
 alias rs-logo='print -P "%F{120}" && cat ~/.dotfiles/ascii-art/rs-logo && print -P "%f"'
 alias soundcloud-logo='print -P "%F{172}" && cat ~/.dotfiles/ascii-art/soundcloud && print -P "%f"'
+# Remove duplicates from "Open with..." context menu
+alias duplo='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
