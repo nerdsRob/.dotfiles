1828aea81bad0ba46d0ec7749a380dfae54e193d
nerdsRob <robert@soundcloud.com>
2013-07-08 13:40:51 +0200

    AppCode alias

--- etc/aliases.zsh
+++ etc/aliases.zsh
@@ -1,7 +1,12 @@
 alias scios="cd ~/Code/SC-urgent"
 alias ll="ls -la -FHG"
+alias ac="open -a AppCode"
 alias webserver='python -m SimpleHTTPServer'
 alias rs-logo='print -P "%F{120}" && cat ~/.dotfiles/ascii-art/rs-logo && print -P "%f"'
 alias soundcloud-logo='print -P "%F{172}" && cat ~/.dotfiles/ascii-art/soundcloud && print -P "%f"'
 # Remove duplicates from "Open with..." context menu
 alias duplo='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
+# xctool run tests and export results to json and open in sublime
+alias xtj='xctool -workspace SoundCloud.xcworkspace -scheme SoundCloud -sdk iphonesimulator5.0 -configuration Debug run-tests -test-sdk iphonesimulator5.0 -freshSimulator -reporter json-stream:xctool-output.json && subl xctool-output.json'
+# xctool run tests
+alias xt='xctool -workspace SoundCloud.xcworkspace -scheme SoundCloud -sdk iphonesimulator5.0 -configuration Debug run-tests -test-sdk iphonesimulator5.0 -freshSimulator'
